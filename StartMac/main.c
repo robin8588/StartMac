29b7e9e13ca8c6e2a2863df634e1d8f4784173c8
Wang Leo <wangleo@LeodeMac.local>
2013-05-09 17:54:28 +0800

    Initial Commit

---
+++ StartMac/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  StartMac
+//
+//  Created by Wang Leo on 13-5-9.
+//  Copyright (c) 2013年 Wang Leo. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
