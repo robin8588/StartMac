164c0d7221e6f85ca207732bc70f9f48b43b37ac
craig8588 <Craig8588@sina.com>
2013-05-13 19:11:19 +0800

    begin start mac

--- StartMac/main.c
+++ StartMac/main.c
@@ -8,11 +8,22 @@
 
 #include <stdio.h>
 
+void singTheSong(int numberOfBottles){
+    if(numberOfBottles==0){
+        printf("There are simply no more bottles of beer on the wall.\n");
+    }else{
+        printf("%d bottles of beer on the wall. %d bottles of beer.\n",numberOfBottles,numberOfBottles);
+        int oneFewer=numberOfBottles-1;
+        printf("Take one down,pass it around,%d bottles of beer on the wall.\n",oneFewer);
+        singTheSong(oneFewer);
+        printf("Put a bootle in the recycling,%d empty bottles in the bin.\n",numberOfBottles);
+    }
+}
+
 int main(int argc, const char * argv[])
 {
-
-    // insert code here...
-    printf("Hello, World!\n");
+    singTheSong(99);
     return 0;
 }
+
 
