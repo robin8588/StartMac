4773a7dc77b744bd5b6ca3d3e17c4d18431db661
craig8588 <Craig8588@sina.com>
2013-05-21 21:35:40 +0800

    Sine

--- StartMac/main.c
+++ StartMac/main.c
@@ -7,19 +7,23 @@
 //
 
 #include <stdio.h>
-
-float remainingAngel(float angleA,float angleB)
-{
-    return 180.0-angleA-angleB;
-}
-
+#include <stdlib.h>
+#include <math.h>
 
 int main(int argc, const char * argv[])
 {
-    float angleA=30.0;
-    float angleB=60.0;
-    float angleC=remainingAngel(angleA, angleB);
-    printf("The third angle is %.2f\n",angleC);
+    printf(" 3*3+5*2=%d\n",3*3+5*2);
+    printf("11/3=%d remainder of %d \n",11/3,11%3);
+    printf("11/3.0=%f\n",11/(float)3);
+    printf("The absolute value of -5 is %d\n",abs(-5));
+    
+    double y= 123.456;
+    printf("y is %.2f\n",y);
+    printf("y is %.2e\n",y);
+    
+    float i=1;
+    float f=sinf(i);
+    printf("the sine of 1 radian is %.3f\n",f);
     return 0;
 }
 
