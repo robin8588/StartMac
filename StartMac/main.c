b5fb1773016a341c34c08788f4e1544f35237108
craig8588 <Craig8588@sina.com>
2013-06-06 17:04:43 +0800

    Stack

--- StartMac/main.c
+++ StartMac/main.c
@@ -7,15 +7,15 @@
 //
 
 #include <stdio.h>
-#include <time.h>
+#include <stdlib.h>
 
 int main(int argc, const char * argv[])
 {
-    long secondsSince1970=time(NULL);
-    long after4millionseconds =4000000+secondsSince1970;
-    struct tm whatDate;
-    localtime_r(&after4millionseconds, &whatDate);
-    printf("The date in 4 million seconds will be %d - %d -%d  %d : %d : %d",whatDate.tm_year+1900,whatDate.tm_mon+1,whatDate.tm_mday,whatDate.tm_hour,whatDate.tm_min,whatDate.tm_sec);
+    float *startOfBuffer;
+    
+    startOfBuffer =malloc(1000*sizeof(float));
+    free(startOfBuffer);
+    startOfBuffer=NULL;
     return 0;
 }
 
